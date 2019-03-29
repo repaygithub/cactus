253b0b554e3b6a62307e5f912dc20e72d40314c9
Dhalton <dhuber@repay.com>
2019-03-29 11:48:46 -0700

    test(cactus-web): configure jest & add button tests

--- modules/cactus-web/src/Button/Button.test.tsx
+++ modules/cactus-web/src/Button/Button.test.tsx
@@ -1,19 +1,85 @@
 import * as React from 'react'
 import { cleanup, render, fireEvent } from 'react-testing-library'
 import Button from './Button'
-import { ThemeProvider } from 'styled-components'
 import cactusTheme from '@repay/cactus-theme'
+import { ThemeProvider } from 'styled-components'
 
 afterEach(cleanup)
 
 describe('component: Button', () => {
-  test('snapshot', () => {
-    const { container } = render(
+  test('should default to standard variant', () => {
+    const button = render(
       <ThemeProvider theme={cactusTheme}>
-        <Button />
+        <Button>
+          Click me!
+        </Button>
       </ThemeProvider>
     )
 
-    expect(container).toMatchSnapshot()
+    expect(button).toMatchSnapshot()
+  })
+
+  test('should render standard variant', () => {
+    const button = render(
+      <ThemeProvider theme={cactusTheme}>
+        <Button variant="standard">
+          Click me!
+        </Button>
+      </ThemeProvider>
+    )
+
+    expect(button).toMatchSnapshot()
+  })
+
+  test('should render call to action variant', () => {
+    const button = render(
+      <ThemeProvider theme={cactusTheme}>
+        <Button variant="action">
+          Click me!
+        </Button>
+      </ThemeProvider>
+    )
+
+    expect(button).toMatchSnapshot()
+  })
+
+  test('should render disabled variant', () => {
+    const button = render(
+      <ThemeProvider theme={cactusTheme}>
+        <Button disabled>
+          Click me!
+        </Button>
+      </ThemeProvider>
+    )
+
+    expect(button).toMatchSnapshot()
+  })
+
+  test('should trigger onClick', () => {
+    const onClick = jest.fn()
+    const { getByTestId } = render(
+      <ThemeProvider theme={cactusTheme}>
+        <Button onClick={onClick} variant="action" data-testid="clicked">
+          Click me!
+        </Button>
+      </ThemeProvider>
+    )
+
+    fireEvent.click(getByTestId('clicked'))
+    expect(onClick).toHaveBeenCalled()
+  })
+
+  test('should not trigger disabled onClick', () => {
+    const onClick = jest.fn()
+    const { getByTestId } = render(
+      <ThemeProvider theme={cactusTheme}>
+        <Button onClick={onClick} variant="action" disabled data-testid="not-clicked">
+          Click me!
+        </Button>
+      </ThemeProvider>
+    )
+
+    fireEvent.click(getByTestId('not-clicked'))
+    expect(onClick).not.toHaveBeenCalled()
   })
 })
