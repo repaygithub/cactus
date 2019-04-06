75744b343d8806af529786b0eb796155e6d08e69
Dhalton <dhuber@repay.com>
2019-04-05 17:15:03 -0700

    feat(cactus-web): Add icon buttons CACTUS-21

---
+++ modules/cactus-web/src/IconButton/IconButton.test.tsx
@@ -0,0 +1,170 @@
+import * as React from 'react'
+import { cleanup, render, fireEvent } from 'react-testing-library'
+import cactusTheme from '@repay/cactus-theme'
+import IconButton from './IconButton'
+import { StatusCheck } from '@repay/cactus-icons'
+import { ThemeProvider } from 'styled-components'
+
+afterEach(cleanup)
+
+describe('component: IconButton', () => {
+  test('should default to standard medium icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton>
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render standard medium icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton iconSize="medium">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render tiny icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton iconSize="tiny">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render small icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton iconSize="small">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render large icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton iconSize="large">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render call to action icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton variant="action">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render inverse standard icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton variant="standard" inverse>
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render inverse call to action icon button', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton variant="action" inverse>
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render icon button with aria-label', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton label="uchiha-itachi">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render disabled variant', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton disabled>
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should render inverse disabled variant', () => {
+    const iconButton = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton inverse disabled>
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    expect(iconButton.asFragment()).toMatchSnapshot()
+  })
+
+  test('should trigger onClick', () => {
+    const onClick = jest.fn()
+    const { getByTestId } = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton onClick={onClick} data-testid="clicked">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    fireEvent.click(getByTestId('clicked'))
+    expect(onClick).toHaveBeenCalled()
+  })
+
+  test('should not trigger onClick', () => {
+    const onClick = jest.fn()
+    const { getByTestId } = render(
+      <ThemeProvider theme={cactusTheme}>
+        <IconButton disabled onClick={onClick} data-testid="not-clicked">
+          <StatusCheck />
+        </IconButton>
+      </ThemeProvider>
+    )
+
+    fireEvent.click(getByTestId('not-clicked'))
+    expect(onClick).not.toHaveBeenCalled()
+  })
+})
---
+++ modules/cactus-web/src/IconButton/IconButton.tsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import styled, { FlattenInterpolation, ThemeProps, css } from 'styled-components'
+import { CactusTheme } from '@repay/cactus-theme'
+
+export type IconButtonVariants = 'standard' | 'action'
+export type IconButtonSizes = 'tiny' | 'small' | 'medium' | 'large'
+
+interface IconButtonProps extends React.HTMLProps<HTMLButtonElement> {
+  iconSize?: IconButtonSizes
+  variant?: IconButtonVariants
+  disabled?: boolean
+  label?: string
+  display?: 'flex' | 'inline-flex'
+  inverse?: boolean
+}
+
+type SizeMap = { [K in IconButtonSizes]: string }
+
+const sizeMap: SizeMap = {
+  tiny: '8px',
+  small: '16px',
+  medium: '24px',
+  large: '40px',
+}
+
+type VariantMap = { [K in IconButtonVariants]: FlattenInterpolation<ThemeProps<CactusTheme>> }
+
+const variantMap: VariantMap = {
+  action: css`
+    color: ${p => p.theme.colors.callToAction};
+
+    &:hover,
+    &:focus {
+      color: ${p => p.theme.colors.base};
+    }
+  `,
+  standard: css`
+    color: ${p => p.theme.colors.base};
+
+    &:hover,
+    &:focus {
+      color: ${p => p.theme.colors.callToAction};
+    }
+  `,
+}
+
+const inverseVariantMap: VariantMap = {
+  action: css`
+    color: ${p => p.theme.colors.callToAction};
+
+    &:hover,
+    &:focus {
+      color: ${p => p.theme.colors.white};
+    }
+  `,
+  standard: css`
+    color: ${p => p.theme.colors.white};
+
+    &:hover,
+    &:focus {
+      color: ${p => p.theme.colors.base};
+      background: ${p => p.theme.colors.white};
+    }
+  `,
+}
+
+const disabled: FlattenInterpolation<ThemeProps<CactusTheme>> = css`
+  color: ${p => p.theme.colors.mediumGray};
+  cursor: not-allowed;
+`
+
+const variantOrDisabled = (
+  props: IconButtonProps
+): FlattenInterpolation<ThemeProps<CactusTheme>> | undefined => {
+  const map = props.inverse ? inverseVariantMap : variantMap
+  if (props.disabled) {
+    return disabled
+  } else if (props.variant !== undefined) {
+    return map[props.variant]
+  }
+}
+
+const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
+  const { label, children, inverse, iconSize, ...buttonProps } = props
+
+  return (
+    <button aria-label={label} ref={ref} {...buttonProps}>
+      {children}
+    </button>
+  )
+})
+
+IconButton.defaultProps = {
+  variant: 'standard',
+  iconSize: 'medium',
+  disabled: false,
+  inverse: false,
+}
+
+export default styled(IconButton)<IconButtonProps>`
+  display: ${p => p.display || 'inline-flex'};
+  font-size: ${p => p.iconSize !== undefined && sizeMap[p.iconSize]};
+  align-items: center;
+  justify-content: center;
+  padding: 0px;
+  border-radius: 50%;
+  border: none;
+  background: transparent;
+  outline: none;
+  cursor: pointer;
+  ${variantOrDisabled}
+`
