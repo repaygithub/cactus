05bcb3fee0e28603ce534c0b84f0f47a8d365565
Dhalton <dhuber@repay.com>
2019-04-17 16:12:05 -0700

    chore(cactus-web): Show how to manage checkbox state using props

--- modules/cactus-web/src/CheckBox/CheckBox.story.tsx
+++ modules/cactus-web/src/CheckBox/CheckBox.story.tsx
@@ -8,5 +8,15 @@ const checkBoxStories = storiesOf('CheckBox', module)
 const eventLoggers = actions('onChange', 'onFocus', 'onBlur')
 
 checkBoxStories.add('Basic Usage', () => (
-  <CheckBox id="test" disabled={boolean('disabled', false)} {...eventLoggers} />
+  <CheckBox id="test" name="kaneki" disabled={boolean('disabled', false)} {...eventLoggers} />
+))
+
+checkBoxStories.add('Controlling Value Through Props', () => (
+  <CheckBox
+    id="test"
+    name="touka"
+    disabled={boolean('disabled', false)}
+    checked={boolean('checked', false)}
+    {...eventLoggers}
+  />
 ))
