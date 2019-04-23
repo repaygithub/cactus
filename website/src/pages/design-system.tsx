b7574c0807a523242e55e3aa45c04760936cf657
JamesNimlos <jnimlos@repay.com>
2019-04-23 14:52:08 -0700

    fix(docs-website): resolve to single @types/react to remove type conficts

--- website/src/pages/design-system.tsx
+++ website/src/pages/design-system.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
-import Box from '../../components/Box'
 
 const Em = styled.em`
   font-style: normal;
