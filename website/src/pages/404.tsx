b83d385a8154b8c8a5b2500aa9375857b02572ed
JamesNimlos <jnimlos@repay.com>
2019-04-25 14:14:34 -0700

    fix(website): fix ts def for Box component

--- website/src/pages/404.tsx
+++ website/src/pages/404.tsx
@@ -13,7 +13,7 @@ export default () => (
     >
       <h1>Page Not Found</h1>
       <p>Use the menu to the left to roam the available pages.</p>
-      <Box position="absolute" left="0" top="0" right="0" zIndex="-1">
+      <Box position="absolute" left="0" top="0" right="0" zIndex={-1}>
         <NoResultsCactus />
       </Box>
     </Box>
