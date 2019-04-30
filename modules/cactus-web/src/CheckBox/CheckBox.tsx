b0e5e629fe96b2552a7b528c15e1ca9e381576d5
Dhalton <dhuber@repay.com>
2019-04-30 11:42:12 -0700

    refactor(cactus-web): Utilize DetailedHTMLProps on CheckBox

--- modules/cactus-web/src/CheckBox/CheckBox.tsx
+++ modules/cactus-web/src/CheckBox/CheckBox.tsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 import { StatusCheck } from '@repay/cactus-icons'
 import { Omit } from '../types'
 
-export interface CheckBoxProps extends Omit<React.HTMLProps<HTMLInputElement>, 'ref' | 'as'> {
+export interface CheckBoxProps
+  extends Omit<
+    React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+    'ref'
+  > {
   id: string
   disabled?: boolean
 }
@@ -12,7 +16,8 @@ interface StyledCheckBoxProps extends React.HTMLProps<HTMLDivElement> {
   disabled?: boolean
 }
 
-interface CheckBoxContainerProps extends React.HTMLProps<HTMLLabelElement> {
+interface CheckBoxContainerProps
+  extends React.DetailedHTMLProps<React.LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement> {
   disabled?: boolean
 }
 
