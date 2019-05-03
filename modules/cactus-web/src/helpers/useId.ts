8228b2b9ff51983b0d6451512b38eb678b72c39c
JamesNimlos <jnimlos@repay.com>
2019-05-03 11:03:22 -0700

    feat(cactus-web): export useId hook

--- modules/cactus-web/src/helpers/useId.ts
+++ modules/cactus-web/src/helpers/useId.ts
@@ -1,7 +1,7 @@
 import { useRef } from 'react'
 import generateId from './generateId'
 
-const useId = (customId: string | undefined, prefix: string = 'rand'): string => {
+const useId = (customId?: string | undefined, prefix: string = 'rand'): string => {
   const idRef = useRef<string | null>(customId || null)
   if (idRef.current === null) {
     idRef.current = generateId(prefix)
