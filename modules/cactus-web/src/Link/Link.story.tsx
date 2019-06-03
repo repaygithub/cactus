9ca90d3e497cfff11264123d0df9caf9a9a9b3b4
Dhalton <dhuber@repay.com>
2019-06-03 11:04:08 -0700

    chore(cactus-web): Add experimental link styles to storybook

--- modules/cactus-web/src/Link/Link.story.tsx
+++ modules/cactus-web/src/Link/Link.story.tsx
@@ -2,6 +2,42 @@ import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 import Link from './Link'
 import React from 'react'
+import styled from 'styled-components'
+
+const ExperimentalLink = styled(Link)`
+  text-decoration: none;
+  background-size: 100% 0.5em;
+  background-repeat: no-repeat;
+
+  :link {
+    background-image: linear-gradient(
+      ${p => p.theme.colors.callToAction},
+      ${p => p.theme.colors.callToAction}
+    );
+    background-position: left 0 bottom -40%;
+  }
+
+  :visited {
+    color: ${p => p.theme.colors.base};
+    background-image: linear-gradient(${p => p.theme.colors.base}, ${p => p.theme.colors.base});
+    background-position: left 0 bottom -40%;
+  }
+
+  :hover {
+    color: ${p => p.theme.colors.base};
+    background-image: linear-gradient(${p => p.theme.colors.base}, ${p => p.theme.colors.base});
+    background-position: left 0 bottom -40%;
+  }
+
+  :focus {
+    color: ${p => p.theme.colors.base};
+    background-image: linear-gradient(${p => p.theme.colors.base}, ${p => p.theme.colors.base});
+    background-position: left 0 bottom -30%
+    ::after {
+      display: none;
+    }
+  }
+`
 
 storiesOf('Link', module)
   .add('Basic Usage', () => (
@@ -20,4 +56,9 @@ storiesOf('Link', module)
       To review the cactus documentation site,{' '}
       <Link to="https://repaygithub.github.io/cactus/">click here</Link>.
     </span>
+  ))
+  .add('Experimental Solution', () => (
+    <ExperimentalLink to={text('to', 'https://repaygithub.github.io/cactus/')}>
+      Click me!
+    </ExperimentalLink>
   ))
