d1fd970a3bf705c5f21218554297f484b0e490da
JamesNimlos <jnimlos@repay.com>
2019-06-17 11:46:47 -0700

    feat(cactus-theme): update spaces

--- modules/cactus-theme/index.ts
+++ modules/cactus-theme/index.ts
@@ -163,7 +163,7 @@ export function generateTheme({ primaryHue }: GeneratorOptions = repayOptions): 
       transparentError,
       transparentWarning,
     },
-    space: [0, 2, 4, 8, 16, 32, 64],
+    space: [0, 2, 4, 8, 16, 24, 32, 40],
     fontSizes,
     iconSizes,
     textStyles: {
