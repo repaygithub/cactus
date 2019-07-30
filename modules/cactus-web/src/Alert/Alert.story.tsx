8036ce1376d3e5308c3b818daf8625fe2071fbd2
vvyverman1 <vvyverman@repay.com>
2019-07-30 09:05:09 -0700

    Add a Story with a drop shadow on alert

--- modules/cactus-web/src/Alert/Alert.story.tsx
+++ modules/cactus-web/src/Alert/Alert.story.tsx
@@ -4,10 +4,16 @@ import { select, text } from '@storybook/addon-knobs/react'
 import { storiesOf } from '@storybook/react'
 import Alert, { Status, Type } from './Alert'
 import React from 'react'
+import styled from 'styled-components'
 
 const status: Status[] = ['error', 'warning', 'info', 'success']
 const type: Type[] = ['general', 'push']
 const eventLoggers = actions('onClose')
+
+const ShadowFlex = styled(Flex)`
+  box-shadow: 0 9px 24px ${p => p.theme.colors.callToAction};
+  border-radius: 8px;
+`
 
 storiesOf('Alert', module).add('Basic Usage', () => {
   return (
@@ -22,7 +28,7 @@ storiesOf('Alert', module).add('Basic Usage', () => {
     </Flex>
   )
 })
-storiesOf('Alert', module).add('Small Alert', () => {
+storiesOf('Alert', module).add('Small Push Alert', () => {
   return (
     <Flex width="320px">
       <Alert
@@ -33,5 +39,18 @@ storiesOf('Alert', module).add('Small Alert', () => {
         {text('Message', 'Message goes here')}
       </Alert>
     </Flex>
+  )
+})
+storiesOf('Alert', module).add('Push Alert With Shadow', () => {
+  return (
+    <ShadowFlex width="320px">
+      <Alert
+        status={select('Status', status, 'error')}
+        type={select('Type', type, 'push')}
+        {...eventLoggers}
+      >
+        {text('Message', 'Message goes here')}
+      </Alert>
+    </ShadowFlex>
   )
 })
