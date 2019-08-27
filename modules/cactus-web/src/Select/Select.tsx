ab992f52457ff3d5747964bf35141b926899c51e
Dhalton <dhuber@repay.com>
2019-08-27 13:56:21 -0700

    fix(cactus-web): Fixed Select when value passed is not in options CACTUS-172

--- modules/cactus-web/src/Select/Select.tsx
+++ modules/cactus-web/src/Select/Select.tsx
@@ -591,8 +591,12 @@ class SelectBase extends React.Component<SelectProps, SelectState> {
                 <Rect observe={isOpen}>
                   {({ ref: listRef, rect: listRect }) => {
                     const selected = selectedValue !== null ? selectedValue : value
-                    const activeDescendant =
-                      selected && isOpen ? optionsMap[selected].id : undefined
+                    let activeDescendant: string | undefined = undefined
+                    if (selected !== null && isOpen) {
+                      activeDescendant = optionsMap.hasOwnProperty(selected)
+                        ? optionsMap[selected].id
+                        : optionsMap[Object.keys(optionsMap)[0]].id
+                    }
                     return (
                       <List
                         onBlur={this.handleListBlur}
@@ -611,7 +615,7 @@ class SelectBase extends React.Component<SelectProps, SelectState> {
                       >
                         {options.map(o => {
                           const opt = optionsMap[o.value]
-                          const isSelected = selected === opt.value || undefined
+                          const isSelected = activeDescendant === opt.id || undefined
                           return (
                             <Option
                               id={opt.id}
