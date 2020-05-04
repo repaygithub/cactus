227e62b469aca1e17785b8e955ecc4200f3b21c4
Dhalton <dhuber@repay.com>
2020-05-04 16:26:07 -0700

    chore(cactus-web): Add css loaders & jest css mocks for packages that need it

--- examples/mock-ebpp/webpack.config.js
+++ examples/mock-ebpp/webpack.config.js
@@ -1,9 +1,4 @@
-const path = require('path')
-
 module.exports = function(config) {
-  // ensure react resolves to a single location
-  config.resolve.alias.react = path.resolve('../..', 'node_modules', 'react')
-
   config.module.rules.push({
     test: /\.css$/i,
     use: {
