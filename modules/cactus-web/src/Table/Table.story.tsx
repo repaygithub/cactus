c4ccca693761ae97ed75948860052c7d42c82ffb
Glen Phelps <gphelps@repayonline.com>
2020-07-01 14:27:43 -0700

    feat(cactus-web): add "card" variant to table component
    CACTUS-258

--- modules/cactus-web/src/Table/Table.story.tsx
+++ modules/cactus-web/src/Table/Table.story.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { boolean, number, select, text } from '@storybook/addon-knobs'
 import { DefaultTheme, StyledComponent } from 'styled-components'
 import { storiesOf } from '@storybook/react'
+import ScreenSizeProvider from '../ScreenSizeProvider/ScreenSizeProvider'
 import Table from './Table'
 
 type CellAlignment = 'left' | 'right' | 'center'
@@ -13,10 +14,19 @@ const alignOptions = {
   undefined: '',
 }
 
+type Variant = 'table' | 'card'
+const varOptions = {
+  undefined: '',
+  table: 'table',
+  card: 'card',
+}
+
 const createAlignKnob = () =>
   select('align (prop on Table.Cell)', alignOptions, 'left') as CellAlignment
 
-storiesOf('Table', module).add('Layout', () => {
+const tableStories = storiesOf('Table', module)
+
+tableStories.add('Layout', () => {
   const fullWidth = boolean('fullWidth', true)
   const captionText = text('Caption', '')
   const hasHeader = boolean('Has Header', true)
@@ -26,15 +36,16 @@ storiesOf('Table', module).add('Layout', () => {
   const rowCount = number('# Rows', 3)
   const alignment = createAlignKnob()
   const hasBody = boolean('Has tbody', true)
+  const variant = select('Variant Override', varOptions, undefined) as Variant
 
-  const makeRow = (
-    content: string,
-    index: number,
-    Row: StyledComponent<any, DefaultTheme, {}, never> = Table.Row
-  ) => {
+  const makeRow = (content: string, index: number, Row: React.ReactType = Table.Row) => {
     const cols = []
     for (let i = 0; i < colCount; i++) {
-      cols.push(<Table.Cell align={alignment} key={i}>{`${content} ${i}`}</Table.Cell>)
+      cols.push(
+        <Table.Cell align={alignment} key={i}>
+          {content ? `${content} ${i}` : null}
+        </Table.Cell>
+      )
     }
     return <Row key={index}>{cols}</Row>
   }
@@ -47,10 +58,48 @@ storiesOf('Table', module).add('Layout', () => {
   }
 
   return (
-    <Table fullWidth={fullWidth}>
-      {captionText && <caption>{captionText}</caption>}
-      {header}
-      <Body>{rows}</Body>
-    </Table>
+    <ScreenSizeProvider>
+      <Table fullWidth={fullWidth} variant={variant}>
+        {captionText && <caption>{captionText}</caption>}
+        {header}
+        <Body>{rows}</Body>
+      </Table>
+    </ScreenSizeProvider>
+  )
+})
+
+tableStories.add('Styles Only', () => {
+  const fullWidth = boolean('fullWidth', true)
+  const captionText = text('Caption', '')
+  const hasHeader = boolean('Has Header', true)
+  const headerText = text('Header Text', 'Header')
+  const cellText = text('Cell Text', 'Cell')
+  const colCount = number('# Columns', 4)
+  const rowCount = number('# Rows', 3)
+  const hasBody = boolean('Has tbody', true)
+
+  const makeRow = (content: string, index: number, Cell: React.ReactType = 'td') => {
+    const cols = []
+    for (let i = 0; i < colCount; i++) {
+      cols.push(<Cell key={i}>{`${content} ${i}`}</Cell>)
+    }
+    return <tr key={index}>{cols}</tr>
+  }
+
+  const header = hasHeader ? <thead>{makeRow(headerText, 0, 'th')}</thead> : null
+  const Body = hasBody ? 'tbody' : React.Fragment
+  const rows = []
+  for (let i = 0; i < rowCount; i++) {
+    rows.push(makeRow(cellText, i + 1))
+  }
+
+  return (
+    <ScreenSizeProvider>
+      <Table as="table" fullWidth={fullWidth}>
+        {captionText && <caption>{captionText}</caption>}
+        {header}
+        <Body>{rows}</Body>
+      </Table>
+    </ScreenSizeProvider>
   )
 })
--- modules/cactus-web/src/Table/Table.tsx
+++ modules/cactus-web/src/Table/Table.tsx
@@ -1,59 +1,197 @@
-import { border } from '../helpers/theme'
-import { Shape } from '@repay/cactus-theme'
+import { border, boxShadow } from '../helpers/theme'
+import { BorderSize, Shape } from '@repay/cactus-theme'
+import { CactusTheme } from '@repay/cactus-theme'
+import { Omit } from '../types'
+import { ScreenSizeContext, Size, SIZES } from '../ScreenSizeProvider/ScreenSizeProvider'
 import PropTypes from 'prop-types'
-import React, { createContext, FunctionComponent, useContext } from 'react'
-import styled, { StyledComponentType } from 'styled-components'
+import React, { createContext, useContext } from 'react'
+import styled, { css, StyledComponentType } from 'styled-components'
+import variant from '../helpers/variant'
 
-type cellAlignment = 'center' | 'right' | 'left'
+type CellAlignment = 'center' | 'right' | 'left'
 
-type cellType = 'th' | 'td'
+type CellType = 'th' | 'td'
 
 type BorderCorner = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
 
+type TableVariant = 'table' | 'card'
+
 interface TableContextProps {
-  cellType: cellType
+  cellType?: CellType
+  inHeader: boolean
+  headers?: React.ReactNode[]
+  cellIndex: number
+  variant: TableVariant
 }
 
 interface TableProps
-  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableElement>, HTMLTableElement> {
-  className?: string
+  extends Omit<
+    React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableElement>, HTMLTableElement>,
+    'ref'
+  > {
   fullWidth?: boolean
+  cardBreakpoint?: Size
+  variant?: TableVariant
+  as?: React.ReactType
 }
 
 interface TableHeaderProps
-  extends React.DetailedHTMLProps<
-    React.TableHTMLAttributes<HTMLTableSectionElement>,
-    HTMLTableSectionElement
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.TableHTMLAttributes<HTMLTableSectionElement>,
+      HTMLTableSectionElement
+    >,
+    'ref'
   > {
-  className?: string
+  variant?: TableVariant
 }
+
 interface TableCellProps
-  extends React.DetailedHTMLProps<
-    React.TableHTMLAttributes<HTMLTableDataCellElement & HTMLTableHeaderCellElement>,
-    HTMLTableDataCellElement & HTMLTableHeaderCellElement
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.TdHTMLAttributes<HTMLTableDataCellElement> &
+        React.ThHTMLAttributes<HTMLTableHeaderCellElement>,
+      HTMLTableDataCellElement & HTMLTableHeaderCellElement
+    >,
+    'ref'
   > {
-  className?: string
-  align?: cellAlignment
+  variant?: TableVariant
+  align?: CellAlignment
+  as?: CellType
 }
 
-interface TableRowProps
-  extends React.DetailedHTMLProps<
-    React.TableHTMLAttributes<HTMLTableRowElement>,
-    HTMLTableRowElement
-  > {
-  className?: string
-}
-interface TableBodyProps
-  extends React.DetailedHTMLProps<
-    React.TableHTMLAttributes<HTMLTableSectionElement>,
-    HTMLTableSectionElement
-  > {
-  className?: string
+type TableRowProps = React.DetailedHTMLProps<
+  React.TableHTMLAttributes<HTMLTableRowElement>,
+  HTMLTableRowElement
+>
+
+type TableBodyProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLTableSectionElement>,
+  HTMLTableSectionElement
+>
+
+const DEFAULT_CONTEXT: TableContextProps = {
+  inHeader: false,
+  cellIndex: 0,
+  variant: 'table',
 }
 
-const TableContext = createContext<TableContextProps>({
-  cellType: 'td',
-})
+const TableContext = createContext<TableContextProps>(DEFAULT_CONTEXT)
+
+const Wrapper = styled.div<TableProps>`
+  max-width: 100%;
+  overflow-x: auto;
+  margin: 0px 16px;
+  ${(p) => p.fullWidth && 'min-width: calc(100% - 32px)'};
+`
+
+export const Table: React.FC<TableProps> = ({ children, cardBreakpoint, ...props }) => {
+  const size = useContext(ScreenSizeContext)
+  const context: TableContextProps = { ...DEFAULT_CONTEXT, headers: [] }
+  if (props.variant) {
+    context.variant = props.variant
+  } else if (cardBreakpoint && size <= SIZES[cardBreakpoint]) {
+    context.variant = 'card'
+  }
+  props.variant = context.variant
+  return (
+    <TableContext.Provider value={context}>
+      {props.as ? (
+        <StyledTable {...props}>{children}</StyledTable>
+      ) : (
+        <Wrapper fullWidth={props.fullWidth}>
+          <StyledTable {...props}>{children}</StyledTable>
+        </Wrapper>
+      )}
+    </TableContext.Provider>
+  )
+}
+
+export const TableCell: React.FC<TableCellProps> = ({ children, ...props }) => {
+  const context = useContext<TableContextProps>(TableContext)
+  if (context.cellType && !props.as) {
+    props.as = context.cellType
+  }
+  const colSpan = parseInt((props.colSpan as any) || '1')
+
+  if (context.inHeader && context.headers) {
+    context.headers[context.cellIndex] = children
+    context.cellIndex += colSpan
+  } else if (context.variant === 'card') {
+    const headerContent = context.headers && context.headers[context.cellIndex]
+    context.cellIndex += colSpan
+    props.variant = 'card'
+    return (
+      <StyledCell {...props}>
+        {headerContent && <HeaderBox>{headerContent}</HeaderBox>}
+        <ContentBox>{children}</ContentBox>
+      </StyledCell>
+    )
+  }
+
+  props.variant = 'table'
+  return <StyledCell {...props}>{children}</StyledCell>
+}
+
+export const TableHeader: React.FC<TableHeaderProps> = ({ children, ...props }) => {
+  const context = useContext<TableContextProps>(TableContext)
+  return (
+    <TableContext.Provider value={{ ...context, inHeader: true, cellType: 'th', cellIndex: 0 }}>
+      <StyledHeader {...props} variant={context.variant}>
+        <tr>{children}</tr>
+      </StyledHeader>
+    </TableContext.Provider>
+  )
+}
+
+export const TableRow: React.FC<TableRowProps> = ({ children, ...props }) => {
+  const context = useContext<TableContextProps>(TableContext)
+
+  return (
+    <TableContext.Provider value={{ ...context, cellIndex: 0 }}>
+      <tr {...props} tabIndex={0}>
+        {children}
+      </tr>
+    </TableContext.Provider>
+  )
+}
+
+export const TableBody = 'tbody'
+
+const TSWorkaround = Table as any
+TSWorkaround.Header = TableHeader
+TSWorkaround.Cell = TableCell
+TSWorkaround.Row = TableRow
+TSWorkaround.Body = TableBody
+
+interface TableComponent extends React.FC<TableProps> {
+  Header: React.ComponentType<TableHeaderProps>
+  Cell: React.ComponentType<TableCellProps>
+  Row: React.ComponentType<TableRowProps>
+  Body: React.ComponentType<TableBodyProps>
+}
+
+//@ts-ignore
+TableCell.propTypes = {
+  align: PropTypes.oneOf<CellAlignment>(['center', 'right', 'left']),
+  as: PropTypes.elementType,
+}
+
+Table.displayName = 'Table'
+
+//@ts-ignore
+Table.propTypes = {
+  fullWidth: PropTypes.bool,
+  cardBreakpoint: PropTypes.oneOf<Size>(['tiny', 'small', 'medium', 'large', 'extraLarge']),
+  variant: PropTypes.oneOf<TableVariant>(['table', 'card']),
+  as: PropTypes.elementType,
+}
+
+Table.defaultProps = {
+  cardBreakpoint: 'tiny',
+}
+
+export default Table as TableComponent
 
 const shapeMap = {
   square: '1px',
@@ -64,61 +202,78 @@ const shapeMap = {
 const getShape = (shape: Shape, location: BorderCorner) =>
   `border-${location}-radius: ${shapeMap[shape]}`
 
-const Wrapper = styled.div<TableProps>`
+const HeaderBox = styled.div.attrs({ 'aria-hidden': 'true' })`
+  text-transform: uppercase;
+  font-weight: bold;
+  text-align: left;
   max-width: 100%;
-  overflow-x: auto;
-  margin: 0px 16px;
-  ${(p) => p.fullWidth && 'min-width: calc(100% - 32px)'};
+  flex-grow: 1;
+  margin-right: 8px;
+  &:empty {
+    display: none;
+  }
 `
-const TableBase: FunctionComponent<TableProps> = (props) => {
-  const { children, className, fullWidth } = props
 
-  return (
-    <Wrapper fullWidth={fullWidth}>
-      <table className={className}>{children}</table>
-    </Wrapper>
-  )
-}
+const ContentBox = styled.div`
+  flex-grow: 1;
+  text-align: right;
+  max-width: 100%;
+  &:only-child,
+  ${HeaderBox}:empty + & {
+    text-align: center;
+    width: 100%;
+  }
+`
 
-const TableCellBase: FunctionComponent<TableCellProps> = (props) => {
-  const { children, className, align } = props
-  const { cellType: Type } = useContext(TableContext)
+const StyledCell = styled.td(
+  variant({
+    table: css<TableCellProps>`
+      text-align: ${(p) => p.align || 'left'};
+      padding: 16px;
+      min-width: calc(160px * 0.7125);
 
-  return <Type className={className}>{children}</Type>
-}
+      ${(p) =>
+        p.theme.mediaQueries &&
+        `${p.theme.mediaQueries.large} {
+      min-width: calc(160px * 0.875);
+    }`}
 
-const TableHeaderBase: FunctionComponent<TableHeaderProps> = (props) => {
-  const { children, className } = props
+      ${(p) =>
+        p.theme.mediaQueries &&
+        `${p.theme.mediaQueries.extraLarge} {
+      min-width: 160px;
+    }`}
+    `,
+    card: css`
+      && {
+        display: flex;
+        max-width: 100%;
+        flex-flow: row wrap;
+        justify-content: space-between;
+        padding: 8px 16px;
+        :nth-of-type(even) {
+          background-color: ${(p) => p.theme.colors.lightContrast};
+        }
+        :last-child {
+          border-bottom-left-radius: inherit;
+          border-bottom-right-radius: inherit;
+        }
+      }
+    `,
+  })
+)
 
-  return (
-    <TableContext.Provider value={{ cellType: 'th' }}>
-      <thead className={className}>
-        <Table.Row>{children}</Table.Row>
-      </thead>
-    </TableContext.Provider>
-  )
-}
+const headerVariants = variant({
+  card: css`
+    tr {
+      position: absolute;
+      top: auto;
+      left: -10000px;
+    }
+  `,
+})
 
-const TableRowBase: FunctionComponent<TableRowProps> = (props) => {
-  const { children, className } = props
-
-  return (
-    <tr className={className} tabIndex={0}>
-      {children}
-    </tr>
-  )
-}
-
-const TableBodyBase: FunctionComponent<TableBodyProps> = (props) => {
-  const { children, className } = props
-  return (
-    <TableContext.Provider value={{ cellType: 'td' }}>
-      <tbody className={className}>{children}</tbody>
-    </TableContext.Provider>
-  )
-}
-
-export const TableHeader = styled(TableHeaderBase)`
+const StyledHeader = styled.thead<TableHeaderProps>`
   // Lots of specificity to override the first/last row border colors.
   &&&&& th,
   &&&&& td {
@@ -126,45 +281,13 @@ export const TableHeader = styled(TableHeaderBase)`
     border: ${(p) => border(p.theme, 'base')};
     ${(p) => p.theme.colorStyles.base};
   }
+  ${headerVariants}
 `
 
-export const TableBody = styled(TableBodyBase)``
-export const TableCell = styled(TableCellBase)`
-  text-align: ${(p) => p.align || 'left'};
-  padding: 16px;
-  min-width: calc(160px * 0.7125);
-
-  ${(p) =>
-    p.theme.mediaQueries &&
-    `${p.theme.mediaQueries.large} {
-    min-width: calc(160px * 0.875);
-  }`}
-
-  ${(p) =>
-    p.theme.mediaQueries &&
-    `${p.theme.mediaQueries.extraLarge} {
-    min-width: 160px;
-  }`}
-`
-
-const TableRow = styled(TableRowBase)``
-
-type TableComponentType = StyledComponentType<TableProps> & {
-  Header: StyledComponentType<TableHeaderProps>
-  Cell: StyledComponentType<TableCellProps>
-  Row: StyledComponentType<TableRowProps>
-  Body: StyledComponentType<TableBodyProps>
-}
-
-const Table = styled(TableBase)`
-  ${(p) => p.fullWidth && 'min-width: 100%'};
-  border-spacing: 0;
-  color: ${(p) => p.theme.colors.darkestContrast};
+const table = css`
+  display: table;
   ${(p) => p.theme.textStyles.small};
-
-  th {
-    font-weight: bold;
-  }
+  border-spacing: 0;
 
   td,
   th {
@@ -187,7 +310,6 @@ const Table = styled(TableBase)`
   }
 
   &&& tr:hover {
-    cursor: pointer;
     th,
     td {
       border-color: ${(p) => p.theme.colors.callToAction};
@@ -248,15 +370,59 @@ const Table = styled(TableBase)`
       }
     }
   }
-` as TableComponentType
+`
 
-Table.propTypes = {
-  fullWidth: PropTypes.bool,
-}
+const card = css`
+  ${(p) => p.theme.textStyles.tiny};
+  overflow-wrap: break-word;
 
-Table.Header = TableHeader
-Table.Cell = TableCell
-Table.Row = TableRow
-Table.Body = TableBody
+  &,
+  thead,
+  tbody,
+  tfoot {
+    display: inline-flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+  }
 
-export default Table
+  tr {
+    display: flex;
+    flex-flow: column nowrap;
+    min-width: 272px;
+    max-width: 360px;
+    ${(p) => boxShadow(p.theme, 1)};
+    background-color: ${(p) => p.theme.colors.white};
+    border: ${(p) => border(p.theme, 'lightContrast')};
+    border-radius: ${(p) => shapeMap[p.theme.shape]};
+    margin: 4px;
+    outline: 0;
+    :focus {
+      border-color: ${(p) => p.theme.colors.callToAction};
+    }
+  }
+  th,
+  td {
+    display: block;
+    text-align: center;
+    max-width: 100%;
+    padding: 8px 16px;
+    :nth-of-type(even) {
+      background-color: ${(p) => p.theme.colors.lightContrast};
+    }
+    :last-child {
+      border-bottom-left-radius: inherit;
+      border-bottom-right-radius: inherit;
+    }
+  }
+`
+
+const StyledTable = styled.table<TableProps>`
+  ${(p) => p.fullWidth && 'min-width: 100%'};
+  color: ${(p) => p.theme.colors.darkestContrast};
+
+  th {
+    font-weight: bold;
+  }
+
+  ${variant({ table, card })};
+`
