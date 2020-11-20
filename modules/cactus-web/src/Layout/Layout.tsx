4888ffb3a6847e04660c3d1501e86372119a383c
Glen Phelps <gphelps@repayonline.com>
2020-11-20 15:50:48 -0700

    fix(cactus-web): fixes a couple of bugs in Layout element placement
    CACTUS-425

--- modules/cactus-web/src/Layout/Layout.tsx
+++ modules/cactus-web/src/Layout/Layout.tsx
@@ -138,7 +138,7 @@ const wrapperStyle = (p: LayoutProps) => css<LayoutProps>`
     ${styles['flow']};
   }
 
-  ${!p.floatLeft
+  ${p.fixedBottom
     ? `
         display: -ms-grid;
         display: grid;
@@ -182,11 +182,6 @@ const wrapperStyle = (p: LayoutProps) => css<LayoutProps>`
           grid-column-start: 1;
           grid-column-end: 3;
         }
-        & > *:last-child {
-          -ms-grid-row: 4;
-          grid-row-start: 4;
-          grid-row-end: 5;
-        }
         & > *:first-child {
           -ms-grid-row: 1;
           grid-row-start: 1;
@@ -196,6 +191,11 @@ const wrapperStyle = (p: LayoutProps) => css<LayoutProps>`
           -ms-grid-row: 2;
           grid-row-start: 2;
           grid-row-end: 3;
+        }
+        & > *:last-child {
+          -ms-grid-row: 4;
+          grid-row-start: 4;
+          grid-row-end: 5;
         }
         & > .cactus-layout-floatLeft {
           -ms-grid-column: 1;
