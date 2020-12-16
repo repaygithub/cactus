1ae1c8d012a0d11264829f96fd80cb055cc81b1c
Dhalton Huber <dhuber@repay.com>
2020-12-16 11:23:57 -0700

    WIP

---
+++ scripts/blackduck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eo pipefail
+
+bash <(curl -s -L https://detect.synopsys.com/detect.sh) \
+    --detect.source.path=${WORKSPACE} \
+    --detect.yarn.prod.only=true \
+    --detect.project.name="github.com/repaygithub/cactus" \
+    --detect.project.version.name="master" \
+    --detect.code.location.name="cactus" \
+    --detect.project.version.phase="RELEASED" \
+    --blackduck.url="https://repay.blackducksoftware.com" \
+    --blackduck.api.token=${BLACKDUCK_TOKEN} \
+    --detect.project.version.distribution=SAAS \
+    --detect.cleanup=true \
+    --detect.tools.excluded=SIGNATURE_SCAN \
+    --detect.excluded.detector.types=MAVEN
