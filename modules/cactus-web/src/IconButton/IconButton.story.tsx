8ef43ea8423c653d0274758aced8818e1e960202
Danilo P <dpena@repay.com>
2021-01-27 16:31:46 -0500

    feat(cactus-web): Added new variants into iconBtn

--- modules/cactus-web/src/IconButton/IconButton.story.tsx
+++ modules/cactus-web/src/IconButton/IconButton.story.tsx
@@ -7,7 +7,7 @@ import actions from '../helpers/storybookActionsWorkaround'
 import { Grid, Text } from '../index'
 import IconButton, { IconButtonSizes, IconButtonVariants } from './IconButton'
 
-const iconButtonVariants: IconButtonVariants[] = ['standard', 'action', 'danger']
+const iconButtonVariants: IconButtonVariants[] = ['standard', 'action', 'danger', 'warning', 'success']
 
 type IconName = keyof typeof icons
 const iconNames: IconName[] = Object.keys(icons) as IconName[]
@@ -23,7 +23,7 @@ const IconButtonBase = ({ size, disabled }: { size: IconButtonSizes; disabled?: 
   const Icon = icons[iconName] as React.ComponentType<any>
   return (
     <>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <IconButton
           variant="standard"
           inverse={boolean('inverse', false)}
@@ -35,7 +35,7 @@ const IconButtonBase = ({ size, disabled }: { size: IconButtonSizes; disabled?: 
           <Icon />
         </IconButton>
       </Grid.Item>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <IconButton
           variant="action"
           inverse={boolean('inverse', false)}
@@ -47,9 +47,33 @@ const IconButtonBase = ({ size, disabled }: { size: IconButtonSizes; disabled?: 
           <Icon />
         </IconButton>
       </Grid.Item>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <IconButton
           variant="danger"
+          inverse={boolean('inverse', false)}
+          iconSize={size}
+          label="add"
+          disabled={disabled}
+          {...eventLoggers}
+        >
+          <Icon />
+        </IconButton>
+      </Grid.Item>
+      <Grid.Item tiny={2}>
+        <IconButton
+          variant="warning"
+          inverse={boolean('inverse', false)}
+          iconSize={size}
+          label="add"
+          disabled={disabled}
+          {...eventLoggers}
+        >
+          <Icon />
+        </IconButton>
+      </Grid.Item>
+      <Grid.Item tiny={2}>
+        <IconButton
+          variant="success"
           inverse={boolean('inverse', false)}
           iconSize={size}
           label="add"
@@ -65,51 +89,61 @@ const IconButtonBase = ({ size, disabled }: { size: IconButtonSizes; disabled?: 
 export const BasicUsage = (): React.ReactElement => {
   return (
     <Grid justify="center">
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="tiny" margin="0">
           Size
         </Text>
       </Grid.Item>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="tiny" margin="0">
           Standard
         </Text>
       </Grid.Item>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Action
         </Text>
       </Grid.Item>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Danger
         </Text>
       </Grid.Item>
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
+        <Text textStyle="small" margin="0">
+          Warning
+        </Text>
+      </Grid.Item>
+      <Grid.Item tiny={2}>
+        <Text textStyle="small" margin="0">
+          Success
+        </Text>
+      </Grid.Item>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Tiny
         </Text>
       </Grid.Item>
       <IconButtonBase size="tiny" />
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Small
         </Text>
       </Grid.Item>
       <IconButtonBase size="small" />
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Medium
         </Text>
       </Grid.Item>
       <IconButtonBase size="medium" />
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Large
         </Text>
       </Grid.Item>
       <IconButtonBase size="large" />
-      <Grid.Item tiny={3}>
+      <Grid.Item tiny={2}>
         <Text textStyle="small" margin="0">
           Disabled
         </Text>
