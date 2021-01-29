6811d07929871aaac1268ce6c95572f9ac2b8632
Danilo P <dpena@repay.com>
2021-01-29 13:22:27 -0500

    feat(cactus-web): initial approach for Dimmer Component
    This is my initial approach for the dimmer component: an item called DimmableContent that works like
    a container that someone could use to wrap the content that should be dimable when page prop is
    false

---
+++ modules/cactus-web/src/Dimmer/Dimmer.tsx
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+
+export interface DimmerProps {
+  active: boolean
+  page?: boolean
+}
+
+type DimmerType = React.FC<DimmerProps> & {
+  DimmableContent: typeof DimmableContent
+}
+
+const DimmableContent: React.FC = ({ children }) => {
+  return <Container>{children}</Container>
+}
+
+const Dimmer: DimmerType = (props): React.ReactElement => {
+  const { active, page, children } = props
+  return (
+    <DimmerStyled active={active} page={page}>
+      {active && children}
+    </DimmerStyled>
+  )
+}
+
+Dimmer.DimmableContent = DimmableContent
+
+const Container = styled.div`
+  position: relative;
+`
+
+const DimmerStyled = styled.div<DimmerProps>`
+  position: ${(p): string => (p.page ? 'fixed' : 'absolute')};
+  display: ${(p): string => (!p.active ? 'none' : 'flex')};
+  background: rgba(46, 53, 56, 0.9);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  vertical-align: middle;
+  flex-direction: column;
+  justify-content: center;
+  z-index: 1000;
+`
+
+Dimmer.propTypes = {
+  active: PropTypes.bool.isRequired,
+  page: PropTypes.bool,
+}
+
+Dimmer.defaultProps = {
+  active: false,
+  page: false,
+}
+
+DimmableContent.propTypes = {
+  active: PropTypes.bool.isRequired,
+}
+
+DimmableContent.defaultProps = {
+  active: false,
+}
+
+export default Dimmer
