4d4e752d8d08bc809d2113cea4bfcfc5f465767b
Danilo P <dpena@repay.com>
2021-02-02 11:37:02 -0500

    fix(cactus-web): dimmer's Story: removed contentDimmer and Modal with dimmer

--- modules/cactus-web/src/Dimmer/Dimmer.story.tsx
+++ modules/cactus-web/src/Dimmer/Dimmer.story.tsx
@@ -4,46 +4,11 @@ import React, { useState } from 'react'
 import Box from '../Box/Box'
 import Button from '../Button/Button'
 import Flex from '../Flex/Flex'
-import Modal from '../Modal/Modal'
-import Text from '../Text/Text'
 import Dimmer from './Dimmer'
 
 export default {
   title: 'Dimmer',
 } as Meta
-
-export const ContentDimmer = (): React.ReactElement => {
-  const [open, setOpen] = useState(false)
-  return (
-    <>
-      <Flex mx={15} justifyContent="start" alignItems="flex-start" flexDirection="column">
-        <h1>Something</h1>
-        <p>
-          Mauris eu felis fringilla tortor scelerisque tincidunt et quis risus. Proin dui arcu,
-          tincidunt nec rhoncus eu, blandit ut odio. Donec ac tristique felis. Morbi vel urna
-          commodo, efficitur orci in, condimentum augue. Morbi neque felis, consequat sit amet mi
-          eget, imperdiet consectetur augue. Sed dignissim congue ex at vestibulum. Etiam rutrum,
-          mauris rutrum maximus congue, lectus sem molestie est, eget gravida orci ligula tempus
-          dolor.
-        </p>
-        <Dimmer.DimmableContent>
-          <h3>Content Dimmer</h3>
-          <p>
-            Mauris eu felis fringilla tortor scelerisque tincidunt et quis risus. Proin dui arcu,
-            tincidunt nec rhoncus eu, blandit ut odio. Donec ac tristique felis. Morbi vel urna
-            commodo, efficitur orci in, condimentum augue.
-          </p>
-          <Button onClick={() => setOpen(true)}>Activate dimmer!</Button>
-          <Dimmer active={open}>
-            <div>
-              <Button onClick={() => setOpen(false)}>Click me!</Button>
-            </div>
-          </Dimmer>
-        </Dimmer.DimmableContent>
-      </Flex>
-    </>
-  )
-}
 
 export const ActivePageDimmer = (): React.ReactElement => {
   const [open, setOpen] = useState(false)
@@ -61,7 +26,7 @@ export const ActivePageDimmer = (): React.ReactElement => {
           dolor.
         </p>
         <Button onClick={() => setOpen(true)}>Activate dimmer!</Button>
-        <Dimmer active={open} page>
+        <Dimmer active={open}>
           <div>
             <Box>
               <Button onClick={() => setOpen(false)}>Click me!</Button>
@@ -70,27 +35,5 @@ export const ActivePageDimmer = (): React.ReactElement => {
         </Dimmer>
       </Flex>
     </>
-  )
-}
-
-export const ModalWithDimmer = (): React.ReactElement => {
-  const [open, setOpen] = useState(false)
-
-  return (
-    <Flex mx={15} justifyContent="start" alignItems="flex-start" flexDirection="column">
-      <h1>Modal with dimmer</h1>
-      <Button onClick={() => setOpen(true)}>Activate dimmer!</Button>
-      <Dimmer active={open} page>
-        <Modal
-          variant="action"
-          isOpen={open}
-          modalLabel="Modal Label"
-          closeLabel="Close Label"
-          onClose={() => setOpen(false)}
-        >
-          <Text as="h3">This is a modal</Text>
-        </Modal>
-      </Dimmer>
-    </Flex>
   )
 }
