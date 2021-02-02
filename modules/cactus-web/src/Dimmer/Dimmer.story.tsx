c38ac457048028238b1599fbcce3fbb7e7324dd6
Danilo P <dpena@repay.com>
2021-02-02 10:26:46 -0500

    fix(cactus-web): brandbar and Layout were removed from Dimmer.Story

--- modules/cactus-web/src/Dimmer/Dimmer.story.tsx
+++ modules/cactus-web/src/Dimmer/Dimmer.story.tsx
@@ -2,10 +2,8 @@ import { Meta } from '@storybook/react/types-6-0'
 import React, { useState } from 'react'
 
 import Box from '../Box/Box'
-import BrandBar from '../BrandBar/BrandBar'
 import Button from '../Button/Button'
 import Flex from '../Flex/Flex'
-import { Layout } from '../Layout/Layout'
 import Modal from '../Modal/Modal'
 import Text from '../Text/Text'
 import Dimmer from './Dimmer'
@@ -13,9 +11,6 @@ import Dimmer from './Dimmer'
 export default {
   title: 'Dimmer',
 } as Meta
-
-const LOGO =
-  'https://repay-merchant-resources.s3.amazonaws.com/staging/24bd1970-a677-4ca7-a4d2-e328ddd4691b/repay_logo_new.jpg'
 
 export const ContentDimmer = (): React.ReactElement => {
   const [open, setOpen] = useState(false)
@@ -55,22 +50,25 @@ export const ActivePageDimmer = (): React.ReactElement => {
 
   return (
     <>
-      <h1>Page Dimmer</h1>
-      <p>
-        Mauris eu felis fringilla tortor scelerisque tincidunt et quis risus. Proin dui arcu,
-        tincidunt nec rhoncus eu, blandit ut odio. Donec ac tristique felis. Morbi vel urna commodo,
-        efficitur orci in, condimentum augue. Morbi neque felis, consequat sit amet mi eget,
-        imperdiet consectetur augue. Sed dignissim congue ex at vestibulum. Etiam rutrum, mauris
-        rutrum maximus congue, lectus sem molestie est, eget gravida orci ligula tempus dolor.
-      </p>
-      <Button onClick={() => setOpen(true)}>Activate dimmer!</Button>
-      <Dimmer active={open} page>
-        <div>
-          <Box>
-            <Button onClick={() => setOpen(false)}>Click me!</Button>
-          </Box>
-        </div>
-      </Dimmer>
+      <Flex mx={15} justifyContent="start" alignItems="flex-start" flexDirection="column">
+        <h1>Page Dimmer</h1>
+        <p>
+          Mauris eu felis fringilla tortor scelerisque tincidunt et quis risus. Proin dui arcu,
+          tincidunt nec rhoncus eu, blandit ut odio. Donec ac tristique felis. Morbi vel urna
+          commodo, efficitur orci in, condimentum augue. Morbi neque felis, consequat sit amet mi
+          eget, imperdiet consectetur augue. Sed dignissim congue ex at vestibulum. Etiam rutrum,
+          mauris rutrum maximus congue, lectus sem molestie est, eget gravida orci ligula tempus
+          dolor.
+        </p>
+        <Button onClick={() => setOpen(true)}>Activate dimmer!</Button>
+        <Dimmer active={open} page>
+          <div>
+            <Box>
+              <Button onClick={() => setOpen(false)}>Click me!</Button>
+            </Box>
+          </div>
+        </Dimmer>
+      </Flex>
     </>
   )
 }
