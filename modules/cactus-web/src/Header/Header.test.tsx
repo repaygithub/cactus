2f58f808f3d241077fbbccb4a84616350671414f
Danilo P <dpena@repay.com>
2021-03-15 15:12:39 -0500

    test(cactus-web): header Tests
    Unit testing for header component, snapshots and storyshots
    
    CACTUS-521

--- modules/cactus-web/src/Header/Header.test.tsx
+++ modules/cactus-web/src/Header/Header.test.tsx
@@ -1,17 +1,85 @@
 import { render } from '@testing-library/react'
 import * as React from 'react'
 
+import Breadcrumb from '../Breadcrumb/Breadcrumb'
+import Button from '../Button/Button'
 import { StyleProvider } from '../StyleProvider/StyleProvider'
 import Header from './Header'
 
 describe('component: Header', () => {
-  test('snapshot', () => {
+  test('Should render the Header witha a single title', () => {
     const { container } = render(
       <StyleProvider>
-        <Header />
+        <Header>
+          <Header.Title>Hasta la vista, baby</Header.Title>
+        </Header>
+      </StyleProvider>
+    )
+    expect(container).toHaveTextContent('Hasta la vista, baby')
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Should render the Header with Title and Items', () => {
+    const { container } = render(
+      <StyleProvider>
+        <Header>
+          <Header.Title>Heading Title</Header.Title>
+          <Header.Item>
+            <Button>Click me!</Button>
+          </Header.Item>
+          <Header.Item>I'll be back</Header.Item>
+        </Header>
+      </StyleProvider>
+    )
+    expect(container).toMatchSnapshot()
+  })
+  test('Should render the Header with Title, Breadcrumb and Items', () => {
+    const { container } = render(
+      <StyleProvider>
+        <Header>
+          <Header.BreadcrumbRow>
+            <Breadcrumb>
+              <Breadcrumb.Item href="/">Label</Breadcrumb.Item>
+              <Breadcrumb.Item href="/" active>
+                <em>Label</em>
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </Header.BreadcrumbRow>
+          <Header.Title>Heading Title</Header.Title>
+          <Header.Item>
+            <Button>Click me!</Button>
+          </Header.Item>
+          <Header.Item>I'll be back</Header.Item>
+        </Header>
+      </StyleProvider>
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Should render the Header with Title and Items', () => {
+    const { container } = render(
+      <StyleProvider>
+        <Header>
+          <Header.Title>Hasta la vista, baby</Header.Title>
+          <Header.Item>
+            <Button>Click me!</Button>
+          </Header.Item>
+          <Header.Item>I'll be back</Header.Item>
+        </Header>
+      </StyleProvider>
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Header component should change their background color by passing the bgColor prop', () => {
+    const { getByTestId } = render(
+      <StyleProvider>
+        <Header bgColor="white" data-testid="headerComponent"></Header>
       </StyleProvider>
     )
 
-    expect(container).toMatchSnapshot()
+    const rawHeader = getByTestId('headerComponent')
+    const headerStyles = window.getComputedStyle(rawHeader)
+    expect(headerStyles.backgroundColor).toBe('rgb(255, 255, 255)')
   })
 })
