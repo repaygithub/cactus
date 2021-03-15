2c7b910c9a7207386928dad3fc219a2dde9470f0
Danilo P <dpena@repay.com>
2021-03-15 15:11:33 -0500

    feat(cactus-web): header component
    Header component with their subcomponents and story
    
    CACTUS-521

--- modules/cactus-web/src/Header/Header.story.tsx
+++ modules/cactus-web/src/Header/Header.story.tsx
@@ -1,11 +1,108 @@
+import * as icons from '@repay/cactus-icons'
+import { select } from '@storybook/addon-knobs'
 import { Meta } from '@storybook/react/types-6-0'
 import React from 'react'
 
-import Header from './Header'
+import Breadcrumb from '../Breadcrumb/Breadcrumb'
+import Button from '../Button/Button'
+import Link from '../Link/Link'
+import Header, { BackgroundColorVariants } from './Header'
 
 export default {
   title: 'Header',
   component: Header,
 } as Meta
 
-export const BasicUsage = (): React.ReactElement => <Header />
+const bgColorVariants: BackgroundColorVariants[] = ['lightContrast', 'white']
+
+export const BasicUsage = (): React.ReactElement => {
+  const bgSelection = select('Background-color', bgColorVariants, 'lightContrast')
+  return (
+    <Header bgColor={bgSelection}>
+      <Header.Title>Heading Title</Header.Title>
+    </Header>
+  )
+}
+
+export const WithButton = (): React.ReactElement => {
+  const bgSelection = select('Background-color', bgColorVariants, 'lightContrast')
+  const Icon = icons['ActionsAdd'] as React.ComponentType<any>
+
+  return (
+    <Header bgColor={bgSelection}>
+      <Header.Title>Heading Title</Header.Title>
+      <Header.Item>
+        <Button variant="action">
+          <Icon /> Add new configuration
+        </Button>
+      </Header.Item>
+    </Header>
+  )
+}
+
+export const WithBreadcrumbs = (): React.ReactElement => {
+  const Icon = icons['ActionsAdd'] as React.ComponentType<any>
+  const bgSelection = select('Background-color', bgColorVariants, 'lightContrast')
+
+  return (
+    <Header bgColor={bgSelection}>
+      <Header.Title>Heading Title</Header.Title>
+      <Header.BreadcrumbRow>
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Label</Breadcrumb.Item>
+          <Breadcrumb.Item href="/" active>
+            <em>Label</em>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </Header.BreadcrumbRow>
+      <Header.Item>
+        <Button variant="action">
+          <Icon /> Add new configuration
+        </Button>
+      </Header.Item>
+    </Header>
+  )
+}
+export const WithMultipleItems = (): React.ReactElement => {
+  const Icon = icons['ActionsAdd'] as React.ComponentType<any>
+  const bgSelection = select('Background-color', bgColorVariants, 'lightContrast')
+
+  return (
+    <Header bgColor={bgSelection}>
+      <Header.BreadcrumbRow>
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Label</Breadcrumb.Item>
+          <Breadcrumb.Item href="/" active>
+            <em>Label</em>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </Header.BreadcrumbRow>
+      <Header.Item>
+        <Button variant="action">
+          <Icon /> Add new configuration
+        </Button>
+      </Header.Item>
+      <Header.Item>Some text because I like crowded UIs</Header.Item>
+    </Header>
+  )
+}
+
+export const WithGoBackLink = (): React.ReactElement => {
+  const Icon = icons['ActionsAdd'] as React.ComponentType<any>
+  const bgSelection = select('Background-color', bgColorVariants, 'lightContrast')
+
+  return (
+    <Header bgColor={bgSelection}>
+      <Header.BreadcrumbRow>
+        <Link to="https://repaygithub.github.io/cactus/">Go back!</Link>
+      </Header.BreadcrumbRow>
+      <Header.Title>Heading Title</Header.Title>
+      <Header.Item>
+        <Button variant="action">
+          <Icon /> Add new configuration
+        </Button>
+      </Header.Item>
+      <Header.Item>Some text because I like crowded UIs</Header.Item>
+    </Header>
+  )
+}
--- modules/cactus-web/src/Header/Header.tsx
+++ modules/cactus-web/src/Header/Header.tsx
@@ -1,10 +1,112 @@
+import PropTypes from 'prop-types'
+import React, { Children, ReactNode } from 'react'
 import styled from 'styled-components'
 import { margin, MarginProps } from 'styled-system'
 
-interface HeaderProps extends MarginProps {}
+import Text from '../Text/Text'
 
-export const Header = styled.div<HeaderProps>`
+export type BackgroundColorVariants = 'lightContrast' | 'white'
+interface HeaderProps extends MarginProps, React.HTMLAttributes<HTMLDivElement> {
+  bgColor?: BackgroundColorVariants
+  children?: ReactNode
+}
+
+export type HeaderType = React.FC<HeaderProps> & {
+  Item: typeof HeaderItem
+  BreadcrumbRow: typeof HeaderBreadcrumbRow
+  Title: typeof HeaderTitle
+}
+
+export const HeaderItem: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children }) => {
+  return <>{children}</>
+}
+
+export const HeaderTitle: React.FC = ({ children }) => {
+  return (
+    <Text as="h1" textStyle="h2" my="0">
+      {children}
+    </Text>
+  )
+}
+
+export const HeaderBreadcrumbRow: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+  children,
+}) => {
+  return <>{children}</>
+}
+
+export const Header: HeaderType = ({ children, bgColor = 'lightContrast', ...rest }) => {
+  const childrens = Children.toArray(children)
+
+  return (
+    <StyledHeader bgColor={bgColor} {...rest}>
+      <HeaderColumn>
+        {childrens.find(
+          (child: any) => child.type.displayName === 'HeaderBreadcrumbRow' && <>{child}</>
+        )}
+        {childrens.find((child: any) => child.type.displayName === 'HeaderTitle' && <>{child}</>)}
+      </HeaderColumn>
+      {childrens.some((i: any) => i.type.displayName === 'HeaderItem') && (
+        <HeaderColumn>
+          {childrens.filter(
+            (child: any) => child.type.displayName === 'HeaderItem' && <>{child}</>
+          )}
+        </HeaderColumn>
+      )}
+    </StyledHeader>
+  )
+}
+
+Header.displayName = 'Header'
+HeaderBreadcrumbRow.displayName = 'HeaderBreadcrumbRow'
+HeaderTitle.displayName = 'HeaderTitle'
+HeaderItem.displayName = 'HeaderItem'
+
+Header.BreadcrumbRow = HeaderBreadcrumbRow
+Header.Title = HeaderTitle
+Header.Item = HeaderItem
+
+Header.propTypes = {
+  bgColor: PropTypes.oneOf(['white', 'lightContrast']),
+}
+
+export const HeaderColumn = styled.div<{ mainColumn?: boolean }>`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding-top: ${(p) => !p.mainColumn && '24px'};
+  ${(p) => `
+    ${p.theme.mediaQueries?.small}{
+      &:first-child {
+        flex-direction: column; 
+      }
+      &:last-child {
+        flex-direction: row; 
+      }
+      padding-top: 0px;
+    }
+  `};
+`
+export const StyledHeader = styled.header<HeaderProps>`
   ${margin}
+
+  align-items: center;
+  background-color: ${(p) =>
+    p.bgColor ? p.theme.colors[p.bgColor] : p.theme.colors['lightContrast']};
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  justify-content: center;
+  padding: 8px 0;
+  width: 100%;
+
+  ${(p) => `
+  ${p.theme.mediaQueries?.small}{
+    justify-content: space-between;
+    flex-direction: row;
+    padding: 8px 40px;
+    }
+  `}
 `
 
 export default Header
