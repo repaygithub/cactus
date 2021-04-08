8ee72e1d3af03b2ace6a5bfef28c4cd7c62c4dc8
Dhalton Huber <dhuber@repay.com>
2021-04-08 15:46:01 -0700

    test(cactus-web): add Preview tests & generate snapshot/storyshots
    CACTUS-516

---
+++ modules/cactus-web/src/Preview/Preview.test.tsx
@@ -0,0 +1,106 @@
+import { fireEvent, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as React from 'react'
+
+import { StyleProvider } from '../StyleProvider/StyleProvider'
+import Preview from './Preview'
+
+const IMAGES = ['http://placekitten.com/400/450', 'http://placekitten.com/450/250']
+
+describe('component: Preview', () => {
+  test('snapshot', () => {
+    const { container } = render(
+      <StyleProvider>
+        <Preview images={IMAGES} />
+      </StyleProvider>
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+
+  describe('Mouse Interactions', () => {
+    test('Should be able to change the image using the arrows', () => {
+      const { getByAltText, getByLabelText } = render(
+        <StyleProvider>
+          <Preview>
+            {IMAGES.map((src, ix) => (
+              <img src={src} alt={`Cute kitten number ${ix + 1}`} key={ix} />
+            ))}
+          </Preview>
+        </StyleProvider>
+      )
+
+      const leftArrow = getByLabelText('Go to the previous image')
+      const rightArrow = getByLabelText('Go to the next image')
+
+      expect(getByAltText('Cute kitten number 1')).toBeInTheDocument()
+      expect(leftArrow).toBeDisabled()
+      userEvent.click(rightArrow)
+      expect(getByAltText('Cute kitten number 2')).toBeInTheDocument()
+      expect(rightArrow).toBeDisabled()
+      userEvent.click(leftArrow)
+      expect(getByAltText('Cute kitten number 1')).toBeInTheDocument()
+    })
+
+    test('should be able to expand an image', () => {
+      const { getByAltText, getAllByAltText, getByLabelText } = render(
+        <StyleProvider>
+          <Preview>
+            {IMAGES.map((src, ix) => (
+              <img src={src} alt={`Cute kitten number ${ix + 1}`} key={ix} />
+            ))}
+          </Preview>
+        </StyleProvider>
+      )
+
+      const image = getByAltText('Cute kitten number 1')
+      userEvent.click(image)
+      const closeButton = getByLabelText('Close the image')
+      expect(document.activeElement).toBe(closeButton)
+      expect(getAllByAltText('Cute kitten number 1')[1]).toBeInTheDocument()
+    })
+  })
+
+  describe('Keyboard Interactions', () => {
+    test('Should be able to change the image using the arrows', () => {
+      const { getByAltText, getByLabelText } = render(
+        <StyleProvider>
+          <Preview>
+            {IMAGES.map((src, ix) => (
+              <img src={src} alt={`Cute kitten number ${ix + 1}`} key={ix} />
+            ))}
+          </Preview>
+        </StyleProvider>
+      )
+
+      const leftArrow = getByLabelText('Go to the previous image')
+      const rightArrow = getByLabelText('Go to the next image')
+
+      expect(getByAltText('Cute kitten number 1')).toBeInTheDocument()
+      expect(leftArrow).toBeDisabled()
+      fireEvent.keyDown(rightArrow, { key: 'Enter' })
+      expect(getByAltText('Cute kitten number 2')).toBeInTheDocument()
+      expect(rightArrow).toBeDisabled()
+      fireEvent.keyDown(leftArrow, { key: 'Enter' })
+      expect(getByAltText('Cute kitten number 1')).toBeInTheDocument()
+    })
+
+    test('Should be able to expand an image', () => {
+      const { getByAltText, getAllByAltText, getByLabelText } = render(
+        <StyleProvider>
+          <Preview>
+            {IMAGES.map((src, ix) => (
+              <img src={src} alt={`Cute kitten number ${ix + 1}`} key={ix} />
+            ))}
+          </Preview>
+        </StyleProvider>
+      )
+
+      const image = getByAltText('Cute kitten number 1')
+      fireEvent.keyDown(image, { key: 'Enter' })
+      const closeButton = getByLabelText('Close the image')
+      expect(document.activeElement).toBe(closeButton)
+      expect(getAllByAltText('Cute kitten number 1')[1]).toBeInTheDocument()
+    })
+  })
+})
