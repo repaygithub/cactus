46548a3786344b63ab98d1320d8d7ea05cbc077b
Michael Soltow <msoltow@repay.com>
2021-06-10 12:06:03 -0400

    chore: upgrade Repay Scripts, Babel dependencies, and Storybook
    Storybook had to be updated too because we needed to be on v6.2 to support Webpack 5 (which is included in the latest version of Repay Scripts)
    
    CACTUS-645

--- modules/cactus-icons/.storybook/main.js
+++ modules/cactus-icons/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../stories/Icons.story.tsx'],
-  addons: ['@storybook/addon-knobs/register'],
+  addons: ['@storybook/addon-knobs'],
+  core: {
+    builder: 'webpack5',
+  },
 }
