853afa327240b5881f77594b65dac3619a57bf1c
Dhalton Huber <dhuber@repay.com>
2021-06-17 15:01:42 -0700

    test(cactus-web): add mini Table test
    CACTUS-578

--- modules/cactus-web/src/Table/Table.test.tsx
+++ modules/cactus-web/src/Table/Table.test.tsx
@@ -31,4 +31,31 @@ describe('component: Table', (): void => {
     const singleCell = getByTestId('cell')
     expect(singleCell).toHaveStyle('width: 240px')
   })
+
+  test('renders mini table styles', () => {
+    const { getByTestId } = render(
+      <StyleProvider>
+        <Table variant="mini">
+          <Table.Header>
+            <Table.Cell data-testid="header-cell">Header Cell</Table.Cell>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell data-testid="data-cell">Data cell</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Data cell</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Data cell</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </StyleProvider>
+    )
+    const headerCell = getByTestId('header-cell')
+    const dataCell = getByTestId('data-cell')
+    expect(headerCell).toHaveStyle('padding: 8px')
+    expect(dataCell).toHaveStyle('padding: 8px')
+  })
 })
