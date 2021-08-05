b342a964b80e4956efb518b381e8403979068477
Dhalton Huber <dhuber@repay.com>
2021-08-05 11:19:44 -0700

    test(cactus-web): improve BrandBar test coverage
    CACTUS-674

--- modules/cactus-web/src/BrandBar/BrandBar.test.tsx
+++ modules/cactus-web/src/BrandBar/BrandBar.test.tsx
@@ -20,16 +20,30 @@ describe('component: BrandBar', () => {
               </BrandBar.UserMenuItem>
             </BrandBar.UserMenu>
           </BrandBar>
+          <div>Click me</div>
         </StyleProvider>
       )
 
-      userEvent.click(getByText('Test name'))
+      const trigger = getByText('Test name').parentElement as HTMLElement
+
+      userEvent.click(trigger)
+      const settings = getByText('Settings')
+      expect(settings).toBeVisible()
+
+      userEvent.click(getByText('Click me'))
+
+      expect(settings).not.toBeVisible()
+
+      userEvent.click(trigger)
+      userEvent.click(settings)
       await animationRender()
-      userEvent.click(getByText('Settings'))
+
       expect(actionOne).toHaveBeenCalled()
+      expect(getByText('Logout')).not.toBeVisible()
+      expect(document.activeElement).toBe(trigger)
     })
 
-    test('can interact with a dropdown', () => {
+    test('can interact with a dropdown', async () => {
       const firstOptionClick = jest.fn()
       const { getByText } = render(
         <StyleProvider>
@@ -44,12 +58,65 @@ describe('component: BrandBar', () => {
               </ul>
             </BrandBar.Item>
           </BrandBar>
+          <div>Click me</div>
         </StyleProvider>
       )
 
-      userEvent.click(getByText('Test Dropdown'))
-      userEvent.click(getByText('Option 1'))
+      const trigger = getByText('Test Dropdown').parentElement as HTMLElement
+
+      userEvent.click(trigger)
+      const option1 = getByText('Option 1')
+
+      expect(option1).toBeVisible()
+
+      userEvent.click(getByText('Click me'))
+      expect(option1).not.toBeVisible()
+
+      userEvent.click(trigger)
+      userEvent.click(option1)
+      await animationRender()
+
       expect(firstOptionClick).toHaveBeenCalled()
+      expect(getByText('Option 2')).not.toBeVisible()
+      expect(document.activeElement).toBe(trigger)
+    })
+
+    test('item supports custom item selectors', async () => {
+      const { getByText } = render(
+        <StyleProvider>
+          <BrandBar>
+            <BrandBar.Item
+              as={BrandBar.Dropdown}
+              listItemSelector='[role="custom-selector"], [role="custom-selector"] *'
+              label="Test Dropdown"
+            >
+              <ul>
+                <li role="menuitem">Option 1</li>
+                <li>Option 2</li>
+                <li role="custom-selector">Option 3</li>
+              </ul>
+            </BrandBar.Item>
+          </BrandBar>
+        </StyleProvider>
+      )
+
+      const trigger = getByText('Test Dropdown').parentElement as HTMLElement
+
+      userEvent.click(trigger)
+      const option1 = getByText('Option 1')
+      const option2 = getByText('Option 2')
+      const option3 = getByText('Option 3')
+      userEvent.click(option1)
+
+      expect(option1).toBeVisible()
+
+      userEvent.click(option2)
+
+      expect(option2).toBeVisible()
+
+      userEvent.click(option3)
+
+      expect(option3).not.toBeVisible()
     })
   })
 
@@ -69,18 +136,41 @@ describe('component: BrandBar', () => {
         </StyleProvider>
       )
 
-      fireEvent.keyDown(getByText('Test name'), { key: 'Enter' })
-      await animationRender()
+      const trigger = getByText('Test name').parentElement as HTMLElement
 
-      // @ts-ignore
-      fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' })
-      await animationRender()
+      fireEvent.keyDown(trigger, { key: 'Enter' })
 
-      // @ts-ignore
-      fireEvent.keyDown(document.activeElement, { key: 'Enter' })
-      await animationRender()
-      expect(actionOne).not.toHaveBeenCalled()
-      expect(actionTwo).toHaveBeenCalled()
+      const settings = getByText('Settings')
+      const logout = getByText('Logout')
+
+      expect(document.activeElement).toBe(settings)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'End' })
+
+      expect(document.activeElement).toBe(logout)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Home' })
+
+      expect(document.activeElement).toBe(settings)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Escape' })
+
+      expect(settings).not.toBeVisible()
+      expect(document.activeElement).toBe(trigger)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Enter' })
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'ArrowDown' })
+
+      expect(document.activeElement).toBe(logout)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'ArrowUp' })
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Enter' })
+
+      expect(actionOne).toHaveBeenCalled()
+      expect(actionTwo).not.toHaveBeenCalled()
+      expect(getByText('Logout')).not.toBeVisible()
+      expect(document.activeElement).toBe(trigger)
     })
 
     test('can interact with a dropdown', () => {
@@ -101,21 +191,35 @@ describe('component: BrandBar', () => {
         </StyleProvider>
       )
 
-      fireEvent.keyDown(getByText('Test Dropdown'), { key: 'Enter' })
-      const firstOption = getByText('Option 1')
-      expect(document.activeElement).toBe(firstOption)
+      const trigger = getByText('Test Dropdown').parentElement as HTMLElement
+      fireEvent.keyDown(trigger, { key: 'Enter' })
+      const option1 = getByText('Option 1')
+      const option2 = getByText('Option 2')
+      const option3 = getByText('Option 3')
 
-      // @ts-ignore
-      fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' })
+      expect(document.activeElement).toBe(option1)
 
-      // @ts-ignore
-      fireEvent.keyDown(document.activeElement, { key: 'ArrowDown' })
+      fireEvent.keyDown(document.activeElement as Element, { key: 'End' })
 
-      // @ts-ignore
-      fireEvent.keyDown(document.activeElement, { key: 'Enter' })
+      expect(document.activeElement).toBe(option3)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Home' })
+
+      expect(document.activeElement).toBe(option1)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Escape' })
+
+      expect(option2).not.toBeVisible()
+      expect(document.activeElement).toBe(trigger)
+
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Enter' })
+      fireEvent.keyDown(document.activeElement as Element, { key: 'ArrowDown' })
+      fireEvent.keyDown(document.activeElement as Element, { key: 'ArrowDown' })
+      fireEvent.keyDown(document.activeElement as Element, { key: 'Enter' })
 
       expect(thirdOptionClick).toHaveBeenCalled()
-      expect(document.activeElement).toBe(getByText('Test Dropdown').parentElement)
+      expect(option2).not.toBeVisible()
+      expect(document.activeElement).toBe(trigger)
     })
   })
 })
