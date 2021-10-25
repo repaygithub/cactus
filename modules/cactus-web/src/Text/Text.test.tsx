3211f03414b2091abbecf037d0082830cdbd70b0
Dhalton Huber <dhuber@repay.com>
2021-10-25 15:07:35 -0700

    test(cactus-web): remove Span related tests
    CACTUS-751

--- modules/cactus-web/src/Text/Text.test.tsx
+++ modules/cactus-web/src/Text/Text.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import * as React from 'react'
 
 import { StyleProvider } from '../StyleProvider/StyleProvider'
-import { Span, Text } from './Text'
+import { Text } from './Text'
 
 describe('component: Text', (): void => {
   test('exists', (): void => {
@@ -22,30 +22,6 @@ describe('component: Text', (): void => {
         <Text colors="base" fontWeight={600} fontStyle="italic" textAlign="right" textStyle="small">
           Stilton lancashire macaroni cheese.
         </Text>
-      </StyleProvider>
-    )
-
-    expect(container.firstChild).not.toBeNull()
-  })
-})
-
-describe('component: Span', (): void => {
-  test('Exists', (): void => {
-    const { getByText } = render(
-      <StyleProvider>
-        <Span>when the cheese comes out everybody's happy</Span>
-      </StyleProvider>
-    )
-
-    expect(getByText(`when the cheese comes out everybody's happy`)).toBeInTheDocument()
-  })
-
-  test('can set text based properties', (): void => {
-    const { container } = render(
-      <StyleProvider>
-        <Span colors="base" fontWeight={600} fontStyle="italic" textAlign="right" textStyle="h2">
-          Stilton lancashire macaroni cheese.
-        </Span>
       </StyleProvider>
     )
 
