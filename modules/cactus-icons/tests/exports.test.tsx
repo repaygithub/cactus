f9233cbb1688fa400774a9f9cae0fc569b7b9b56
Dhalton Huber <dhuber@repay.com>
2021-10-25 13:32:30 -0700

    test(cactus-icons): update cactus-icons tests
    CACTUS-722

--- modules/cactus-icons/tests/exports.test.tsx
+++ modules/cactus-icons/tests/exports.test.tsx
@@ -1,7 +1,11 @@
-import * as icons from '@repay/cactus-icons'
+import defaultExport, * as namedExports from '@repay/cactus-icons'
 
 describe('Exports', (): void => {
-  test('should match snapshot of exported icons', (): void => {
-    expect(Object.keys(icons)).toMatchSnapshot()
+  test('should match snapshot of named exports', async () => {
+    expect(Object.keys(namedExports)).toMatchSnapshot()
+  })
+
+  test('should match snapshot of default export', async () => {
+    expect(Object.keys(defaultExport)).toMatchSnapshot()
   })
 })
--- modules/cactus-icons/tests/svgs.test.tsx
+++ modules/cactus-icons/tests/svgs.test.tsx
@@ -2,9 +2,9 @@ import { StyleProvider } from '@repay/cactus-web'
 import { render } from '@testing-library/react'
 import React from 'react'
 
-import * as icons from '../ts'
+import icons from '../ts'
 
-const iconEntries = Object.entries(icons).filter(([name]): boolean => name !== 'iconSizes')
+const iconEntries = Object.entries(icons)
 
 test.each(iconEntries)('renders %s', (_, Component): void => {
   const { asFragment } = render(
