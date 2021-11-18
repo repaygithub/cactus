36b643d772ba4c9457f006d2035a5602b5d43e78
Dhalton Huber <dhuber@repay.com>
2021-11-18 14:46:05 -0700

    feat(cactus-web): add `stretch` to `justifyContent` options, support flex item props on Tab
    CACTUS-721

--- modules/cactus-web/src/Tabs/Tabs.story.tsx
+++ modules/cactus-web/src/Tabs/Tabs.story.tsx
@@ -2,6 +2,8 @@ import { Page } from 'puppeteer'
 import React from 'react'
 
 import { Tab, TabController, TabList, TabPanel } from '../'
+import { FlexItemProps } from '../helpers/flexItem'
+import { split } from '../helpers/omit'
 import { Story } from '../helpers/storybook'
 
 const LABELS = [
@@ -33,19 +35,30 @@ export default {
   component: TabList,
 } as const
 
-export const BasicUsage: Story<typeof TabList, { tabCount: number }> = ({ tabCount, ...args }) => {
+export const BasicUsage: Story<typeof TabList, FlexItemProps & { tabCount: number }> = ({
+  tabCount,
+  ...args
+}) => {
   const [active, setActive] = React.useState(-1)
+  const tabListProps = split(args, 'justifyContent', 'fillGaps', 'fullWidth')
+  const tabProps = split(args, 'flex', 'flexGrow', 'flexShrink', 'flexBasis')
 
   const tabs = []
   for (let i = 1; i < tabCount; i++) {
     const label = LABELS[i % LABELS.length]
     tabs.push(
-      <Tab key={i} aria-selected={i === active} onClick={() => setActive(i)} children={label} />
+      <Tab
+        key={i}
+        aria-selected={i === active}
+        onClick={() => setActive(i)}
+        children={label}
+        {...tabProps}
+      />
     )
   }
 
   return (
-    <TabList {...args}>
+    <TabList {...tabListProps}>
       {tabCount > 0 && (
         <Tab
           as="a"
@@ -56,35 +69,57 @@ export const BasicUsage: Story<typeof TabList, { tabCount: number }> = ({ tabCou
             setActive(0)
           }}
           children={LABELS[0]}
+          {...tabProps}
         />
       )}
       {tabs}
     </TabList>
   )
 }
-BasicUsage.args = { tabCount: 6, justifyContent: 'space-evenly' }
+BasicUsage.args = {
+  tabCount: 6,
+  justifyContent: 'space-evenly',
+  flex: '',
+  flexGrow: 0,
+  flexShrink: 0,
+  flexBasis: '',
+}
 
-export const WithController: Story<typeof TabList> = (args) => (
-  <TabController id="the-tabs" initialTabId="the-tabs-name-tab">
-    <TabList {...args}>
-      <Tab name="name">Name</Tab>
-      <Tab name="rank">Rank</Tab>
-      <Tab id="serial-num" panelId="serial-box">
-        Serial #
-      </Tab>
-    </TabList>
-    <TabPanel tab="name" mx={5} my={4}>
-      <h1>Nebby Nebulous</h1>
-    </TabPanel>
-    <TabPanel id="the-tabs-rank-panel" tabId="the-tabs-rank-tab" mx={5} my={4}>
-      <h2>Cloudy Nebula</h2>
-    </TabPanel>
-    <TabPanel id="serial-box" tabId="serial-num" mx={5} my={4}>
-      <p>N3BU14</p>
-    </TabPanel>
-  </TabController>
-)
-WithController.args = { justifyContent: 'space-evenly' }
+export const WithController: Story<typeof TabList, FlexItemProps> = (args) => {
+  const tabListProps = split(args, 'justifyContent', 'fillGaps', 'fullWidth')
+  const tabProps = split(args, 'flex', 'flexGrow', 'flexShrink', 'flexBasis')
+  return (
+    <TabController id="the-tabs" initialTabId="the-tabs-name-tab">
+      <TabList {...tabListProps}>
+        <Tab name="name" {...tabProps}>
+          Name
+        </Tab>
+        <Tab name="rank" {...tabProps}>
+          Rank
+        </Tab>
+        <Tab id="serial-num" panelId="serial-box" {...tabProps}>
+          Serial #
+        </Tab>
+      </TabList>
+      <TabPanel tab="name" mx={5} my={4}>
+        <h1>Nebby Nebulous</h1>
+      </TabPanel>
+      <TabPanel id="the-tabs-rank-panel" tabId="the-tabs-rank-tab" mx={5} my={4}>
+        <h2>Cloudy Nebula</h2>
+      </TabPanel>
+      <TabPanel id="serial-box" tabId="serial-num" mx={5} my={4}>
+        <p>N3BU14</p>
+      </TabPanel>
+    </TabController>
+  )
+}
+WithController.args = {
+  justifyContent: 'space-evenly',
+  flex: '',
+  flexGrow: 0,
+  flexShrink: 0,
+  flexBasis: '',
+}
 WithController.parameters = {
   beforeScreenshot: async (page: Page) => {
     await page.focus('#serial-num')
