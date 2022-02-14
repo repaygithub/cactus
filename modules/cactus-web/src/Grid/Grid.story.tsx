ab58ab187e1307ddd681c474760c3711c9221cf4
Glen Phelps <gphelps@repayonline.com>
2022-02-14 14:37:12 -0700

    refactor(cactus-web): get rid of duplicate code
    Not really that big a deal, but since this one file accounts for 75% of
    the duplicated code in the Sonarqube report, I figured fixing it would
    be a relatively big bang for the buck.
    
    CACTUS-850

--- modules/cactus-web/src/Grid/Grid.story.tsx
+++ modules/cactus-web/src/Grid/Grid.story.tsx
@@ -8,145 +8,49 @@ export default {
   parameters: { controls: { disable: true } },
 } as const
 
+type ColumnNum = React.ComponentProps<typeof Grid.Item>['tiny']
+const getItems = (prefix: string, tiny: ColumnNum, medium: ColumnNum, count = 1) => {
+  const items = []
+  for (let i = 0; i < count; i++) {
+    items.push(
+      <Grid.Item key={prefix + i} tiny={tiny} medium={medium}>
+        <Box height="25px" width="100%" backgroundColor="pink" />
+      </Grid.Item>
+    )
+  }
+  return items
+}
+
 export const BasicUsage = (): React.ReactElement => (
   <Grid>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('00', 3, 1, 12)}
+    {getItems('01', 4, 2, 6)}
+    {getItems('02', 3, 3, 4)}
+    {getItems('03', 4, 4, 3)}
 
-    <Grid.Item tiny={4} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('04', 6, 5)}
+    {getItems('05', 3, 4)}
+    {getItems('06', 3, 3)}
 
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('07', 6, 6, 2)}
 
-    <Grid.Item tiny={4} medium={4}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={4}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={4}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('08', 6, 7)}
+    {getItems('09', 3, 3)}
+    {getItems('10', 3, 2)}
 
-    <Grid.Item tiny={6} medium={5}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={4}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('11', 8, 8)}
+    {getItems('12', 4, 4)}
 
-    <Grid.Item tiny={6} medium={6}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={6} medium={6}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('13', 9, 9)}
+    {getItems('14', 3, 3)}
 
-    <Grid.Item tiny={6} medium={7}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('15', 9, 10)}
+    {getItems('16', 3, 2)}
 
-    <Grid.Item tiny={8} medium={8}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={4} medium={4}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('17', 9, 11)}
+    {getItems('18', 3, 1)}
 
-    <Grid.Item tiny={9} medium={9}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={3}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-
-    <Grid.Item tiny={9} medium={10}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={2}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-
-    <Grid.Item tiny={9} medium={11}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-    <Grid.Item tiny={3} medium={1}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
-
-    <Grid.Item tiny={12} medium={12}>
-      <Box height="25px" width="100%" backgroundColor="pink" />
-    </Grid.Item>
+    {getItems('19', 12, 12)}
   </Grid>
 )
 
@@ -155,79 +59,28 @@ export const PreventGridBlowout = (): React.ReactElement => {
   auctor. Cras interdum vel tortor eget consequat. Aliquam id accumsan eros, a condimentum
   neque. Cras nibh leo, pulvinar eu enim at, sodales molestie enim. Cras a lacinia nunc.
   Nunc rutrum ut leo sed elementum.`
+  const getTextItem = (key: string, tiny: ColumnNum, medium: ColumnNum) => (
+    <Grid.Item key={key} tiny={tiny} medium={medium}>
+      <p style={{ overflow: 'hidden', height: 'auto', margin: '0' }}>
+        {LONG_TEXT.replace(/\s/g, '')}
+      </p>
+    </Grid.Item>
+  )
   return (
     <Grid>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <p style={{ overflow: 'hidden', height: 'auto', margin: '0' }}>
-          {LONG_TEXT.replace(/\s/g, '')}
-        </p>
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={1}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
+      {getItems('0', 3, 1, 6)}
+      {getTextItem('1', 3, 1)}
+      {getItems('2', 3, 1, 5)}
 
-      <Grid.Item tiny={3} medium={3}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={3}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={3}>
-        <p style={{ overflow: 'hidden', height: 'auto', margin: '0' }}>
-          {LONG_TEXT.replace(/\s/g, '')}
-        </p>
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={3}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
+      {getItems('3', 3, 3, 2)}
+      {getTextItem('4', 3, 3)}
+      {getItems('5', 3, 3)}
 
-      <Grid.Item tiny={10} medium={9}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
-      <Grid.Item tiny={2} medium={3}>
-        <p style={{ overflow: 'hidden', height: 'auto', margin: '0' }}>
-          {LONG_TEXT.replace(/\s/g, '')}
-        </p>
-      </Grid.Item>
+      {getItems('6', 10, 9)}
+      {getTextItem('7', 2, 3)}
 
-      <Grid.Item tiny={9} medium={4}>
-        <p style={{ overflow: 'hidden', height: 'auto', margin: '0' }}>
-          {LONG_TEXT.replace(/\s/g, '')}
-        </p>
-      </Grid.Item>
-      <Grid.Item tiny={3} medium={8}>
-        <Box height="25px" width="100%" backgroundColor="pink" />
-      </Grid.Item>
+      {getTextItem('8', 9, 4)}
+      {getItems('9', 3, 8)}
     </Grid>
   )
 }
