7ef7e68e4ea86bf49667046a84b3acd52518c83f
Glen Phelps <gphelps@repay.com>
2022-10-31 11:42:14 -0700

    docs: remove gatsby site
    CACTUS-1054

--- website/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import 'normalize.css'
-
-import React from 'react'
-
-import Layout from './src/components/Layout'
-
-export const wrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>
