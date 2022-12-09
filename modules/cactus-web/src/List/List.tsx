a2e50e1db41434e99d2bf630fcff5f0571dacfda
Glen Phelps <gphelps@repay.com>
2022-12-09 14:33:27 -0700

    refactor(cactus-web): update List to inline styles
    CACTUS-1093

--- modules/cactus-web/src/List/List.tsx
+++ modules/cactus-web/src/List/List.tsx
@@ -2,14 +2,13 @@ import icons from '@repay/cactus-icons'
 import { border } from '@repay/cactus-theme'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
-import { compose, margin, MarginProps } from 'styled-system'
+import { margin, MarginProps } from 'styled-system'
 
 import Flex from '../Flex/Flex'
-import { flexItem, FlexItemProps } from '../helpers/styled'
+import { flexItem, FlexItemProps, withStyles } from '../helpers/styled'
 import Text, { TextProps } from '../Text/Text'
 
-interface ListProps extends MarginProps, FlexItemProps, React.HTMLAttributes<HTMLUListElement> {
+interface ListProps extends MarginProps, FlexItemProps {
   dividers?: boolean
 }
 
@@ -25,25 +24,25 @@ interface ItemHeaderProps extends Omit<TextProps, 'color'> {
 
 // Workaround for a styled-components bug when using `&` in a nested style.
 const nested = (): string => `
-  ${UL} {
+  ${List} {
     margin-top: 8px;
     margin-bottom: 8px;
     margin-left: 24px;
   }
 `
 
-const UL = styled.ul<{ $dividers: boolean }>`
+export const List = withStyles('ul', {
+  displayName: 'List',
+  styles: [margin, flexItem],
+  transitiveProps: ['dividers'],
+})<ListProps>`
   padding: 0;
   margin: 0;
   list-style-type: none;
   ${nested}
 
-  && {
-    ${compose(flexItem, margin)}
-  }
-
   ${(p) =>
-    p.$dividers &&
+    p.dividers &&
     `
     li {
       border-top: ${border(p, 'lightContrast')};
@@ -57,14 +56,6 @@ const UL = styled.ul<{ $dividers: boolean }>`
     cursor: pointer;
   }
 `
-
-export const List = React.forwardRef<HTMLUListElement, ListProps>(
-  ({ children, dividers = false, ...props }, ref) => (
-    <UL $dividers={dividers} {...props} ref={ref}>
-      {children}
-    </UL>
-  )
-)
 
 const ListItem = React.forwardRef<HTMLLIElement, ListItemProps>(
   ({ icon, children, ...props }, ref) => {
@@ -90,7 +81,6 @@ const ItemHeader: React.FC<ItemHeaderProps> = ({ icon, ...props }) => {
   )
 }
 
-List.displayName = 'List'
 ListItem.displayName = 'List.Item'
 ItemHeader.displayName = 'List.ItemHeader'
 
