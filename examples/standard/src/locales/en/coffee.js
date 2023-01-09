960580e58a26751b4e3a3cc2c9e5c908152add4c
Glen Phelps <gphelps@repay.com>
2023-01-09 15:28:22 -0700

    fix(standard): add example to illustrate Select altText caching
    CACTUS-995

--- examples/standard/src/locales/en/coffee.js
+++ examples/standard/src/locales/en/coffee.js
@@ -2,4 +2,11 @@ export default `
 coffee__link-message = Click here to hear about coffee!
 coffee__welcome-message = Let's talk about COFFEE!!
 coffee__description = We love coffee. We drink it every day in the office.
+coffee__likes = I like
+coffee__coffee = coffee
+coffee__caffeine = caffeine
+coffee__brew = brew
+coffee__ink = ink
+coffee__battery-acid = battery acid
+coffee__joe = cup of joe
 `
