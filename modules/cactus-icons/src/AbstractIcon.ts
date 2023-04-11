2aa748215d1cfd7f9557103f6c89fd2982761d34
nickj <nickjiunchetti@gmail.com>
2023-04-11 10:43:44 -0300

    fix: fixed icon name

--- modules/cactus-icons/src/AbstractIcon.ts
+++ modules/cactus-icons/src/AbstractIcon.ts
@@ -12,12 +12,12 @@ const stylePropNames = new Set<string>()
 styleFn.propNames?.forEach((propName) => stylePropNames.add(propName))
 
 // Naming it this way so the generated CSS class will start with "CactusIcon-".
-const AbstractIcon = styled(NullIcon).withConfig({
+const CactusIcon = styled(NullIcon).withConfig({
   shouldForwardProp: (p) => !stylePropNames.has(p),
 })<IconStyleProps>`
   vertical-align: middle;
   ${styleFn}
 `
-AbstractIcon.displayName = 'AbstractIcon'
+CactusIcon.displayName = 'AbstractIcon'
 
-export default AbstractIcon
+export default CactusIcon
