f1acef32a5b2ecbe3b41f6de1a715f4db240d7ae
nickj <nickjiunchetti@gmail.com>
2023-04-12 11:43:43 -0300

    fix: remove console.log

--- modules/cactus-icons/scripts/helpers/kebabToPascal.js
+++ modules/cactus-icons/scripts/helpers/kebabToPascal.js
@@ -8,7 +8,6 @@ function convertKebabToPascal(string) {
   // split the string into an array of words
   const words = string.split(/[^A-Za-z0-9]/)
 
-  console.log(words.length)
   // capitalize the first letter of each word and concatenate them
   const pascalCase = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('')
 
